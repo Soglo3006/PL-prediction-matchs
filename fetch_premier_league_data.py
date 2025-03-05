082e5530aac662760787dc2c85ee807b34389326
Soglo3006 <ablbooh0@gmail.com>
2025-03-05 18:34:48 -0500

    Ajout des fonctions pour faire les predictions

--- fetch_premier_league_data.py
+++ fetch_premier_league_data.py
@@ -1,5 +1,7 @@
 import pandas as pd
+from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
 
 # Charger les données depuis un fichier CSV
 data_2324 = pd.read_csv('season-2324.csv')
@@ -31,6 +33,10 @@ data_2324 = moyenne_Stats(data_2324, 'HomeTeam','HYellow', 'Home_avgYellow')
 data_2324 = moyenne_Stats(data_2324, 'AwayTeam','AYellow', 'Away_avgYellow')
 data_2324 = moyenne_Stats(data_2324, 'HomeTeam','HRed', 'Home_avgRed')
 data_2324 = moyenne_Stats(data_2324, 'AwayTeam','ARed', 'Away_avgRed')
+data_2324 = moyenne_Stats(data_2324, 'HomeTeam','HFouls','Home_avgFouls')
+data_2324 = moyenne_Stats(data_2324, 'AwayTeam','AFouls','Away_avgFouls')
+
+
 
 # ligne et colonne
 # row and column
@@ -58,11 +64,47 @@ def calculate_form(data, team_col, result_col, new_col):
 
 data_2324 = calculate_form(data_2324, 'HomeTeam', 'FullTimeResult', 'home_form')
 data_2324 = calculate_form(data_2324, 'AwayTeam', 'FullTimeResult', 'away_form')
+
+
+
+features = ['Home_avgGoal','Away_avgGoal','Home_avgShot','Away_avgShot','Home_avgCorner',
+            'Away_avgCorner','home_form','away_form','Home_avgFouls','Away_avgFouls',
+            'home_code','away_code','goal_diff']
+
+X = data_2324[features]
+y = data_2324['FullTimeResult']
+
+# Séparation des données en jeu d'entraînement et de test
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1)
+
+# Entraînement du modèle
+model = RandomForestClassifier(n_estimators=100, min_samples_split=10, random_state=1)
+model.fit(X_train, y_train)
+
+# Prédictions et évaluation du modèle
+y_pred = model.predict(X_test)
+accuracy = accuracy_score(y_test, y_pred)
+conf_matrix = confusion_matrix(y_test, y_pred)
+class_report = classification_report(y_test, y_pred)
+
+#print(f"Accuracy: {accuracy:.2f}")
+#print("Matrice de confusion:\n", conf_matrix)
+#print("Rapport de classification:\n", class_report)
+
+def predict_future_match(home_team, away_team, model, data):
+    home_code = data[data['HomeTeam'] == home_team]['home_code'].values[0]
+    away_code = data[data['AwayTeam'] == away_team]['away_code'].values[0]
     
-print(data_2324)
+    match_features = data[features].iloc[-1:].copy()
+    match_features['home_code'] = home_code
+    match_features['away_code'] = away_code
     
+    prediction = model.predict(match_features)[0]
+    return prediction
 
-
-
-
+# Exemple de prédiction
+home_team = 'Everton'
+away_team = 'Chelsea'
+predicted_result = predict_future_match(home_team, away_team, model, data_2324)
+print(f"Prédiction pour {home_team} vs {away_team} : {predicted_result}")
 
