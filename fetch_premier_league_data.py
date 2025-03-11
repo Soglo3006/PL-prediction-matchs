4e3a908c7949fd078f3d68acfc9a5ec5f89b6ea6
Soglo3006 <ablbooh0@gmail.com>
2025-03-11 12:50:38 -0400

    Modification des fonctions

--- fetch_premier_league_data.py
+++ fetch_premier_league_data.py
@@ -77,6 +77,10 @@ def moyenne_domcile(data):
     moyenne_buts_domicile = dict(sorted(Équipe_domicile.items()))
     for moy in moyenne_buts_domicile:
         moyenne_buts_domicile[moy] = round(moyenne_buts_domicile[moy]/19,2)
+    for k in range(len(data)):
+        for nom_équipe in moyenne_buts_domicile:
+            if nom_équipe == data.loc[k,'HomeTeam']:
+                data.loc[k,'moyenne_domcile_buts'] = moyenne_buts_domicile[nom_équipe]
     return moyenne_buts_domicile
 
 def moyenne_exterieru(data):
@@ -90,17 +94,22 @@ def moyenne_exterieru(data):
     moyenne_buts_extérieur = dict(sorted(Équipe_extérieur.items()))
     for moy in moyenne_buts_extérieur :
         moyenne_buts_extérieur[moy] = round(moyenne_buts_extérieur[moy]/19,2)
+    for k in range(len(data)):
+        for nom_équipe in moyenne_buts_extérieur:
+            if nom_équipe == data.loc[k,'HomeTeam']:
+                data.loc[k,'moyenne_exterieur_buts'] = moyenne_buts_extérieur[nom_équipe]
     return moyenne_buts_extérieur
+
 
 def difference_buts(data, moyenne_dom, moyenne_ext):
     dom = moyenne_dom
     ext = moyenne_ext
     for i in range(len(data)):
         if dom[data.loc[i,'HomeTeam']]> ext[data.loc[i,'AwayTeam']] : 
-            data.loc[i,'difference'] = dom[data.loc[i,'HomeTeam']] - ext[data.loc[i,'AwayTeam']]
+            data.loc[i,'difference_moyenne'] = dom[data.loc[i,'HomeTeam']] - ext[data.loc[i,'AwayTeam']]
             data.loc[i,'difference_nom'] = data.loc[i,'HomeTeam']
         else:
-            data.loc[i,'difference'] = ext[data.loc[i,'AwayTeam']] - dom[data.loc[i,'HomeTeam']]
+            data.loc[i,'difference_moyenne'] = ext[data.loc[i,'AwayTeam']] - dom[data.loc[i,'HomeTeam']]
             data.loc[i,'difference_nom'] = data.loc[i,'AwayTeam']
 
 data_2324 = moyenne_Stats(data_2324, 'HomeTeam','HomeGoal', 'Home_avgGoal')
@@ -109,12 +118,12 @@ data_2324 = moyenne_Stats(data_2324, 'HomeTeam','HomeShots', 'Home_avgShot')
 data_2324 = moyenne_Stats(data_2324, 'AwayTeam','AwayShots', 'Away_avgShot')
 data_2324 = moyenne_Stats(data_2324, 'HomeTeam','HCorners', 'Home_avgCorner')
 data_2324 = moyenne_Stats(data_2324, 'AwayTeam','ACorners', 'Away_avgCorner')
-data_2324 = moyenne_Stats(data_2324, 'HomeTeam','HYellow', 'Home_avgYellow')
-data_2324 = moyenne_Stats(data_2324, 'AwayTeam','AYellow', 'Away_avgYellow')
-data_2324 = moyenne_Stats(data_2324, 'HomeTeam','HRed', 'Home_avgRed')
-data_2324 = moyenne_Stats(data_2324, 'AwayTeam','ARed', 'Away_avgRed')
-data_2324 = moyenne_Stats(data_2324, 'HomeTeam','HFouls','Home_avgFouls')
-data_2324 = moyenne_Stats(data_2324, 'AwayTeam','AFouls','Away_avgFouls')
+#data_2324 = moyenne_Stats(data_2324, 'HomeTeam','HYellow', 'Home_avgYellow')
+#data_2324 = moyenne_Stats(data_2324, 'AwayTeam','AYellow', 'Away_avgYellow')
+#data_2324 = moyenne_Stats(data_2324, 'HomeTeam','HRed', 'Home_avgRed')
+#data_2324 = moyenne_Stats(data_2324, 'AwayTeam','ARed', 'Away_avgRed')
+#data_2324 = moyenne_Stats(data_2324, 'HomeTeam','HFouls','Home_avgFouls')
+#data_2324 = moyenne_Stats(data_2324, 'AwayTeam','AFouls','Away_avgFouls')
 data_2324 = calculate_form(data_2324, 'HomeTeam', 'FullTimeResult', 'home_form')
 data_2324 = calculate_form(data_2324, 'AwayTeam', 'FullTimeResult', 'away_form')
 
@@ -122,7 +131,8 @@ avantageDomicile(data_2324)
 difference_buts(data_2324,moyenne_domcile(data_2324),moyenne_exterieru(data_2324))
 
 print(data_2324)
-features = ['Home_avgGoal','Away_avgGoal','Home_avgShot','Away_avgShot', 'home_form','away_form']
+features = ['Home_avgGoal','Away_avgGoal','Home_avgShot','Away_avgShot'
+            , 'home_form','away_form', 'home_advantage','difference_moyenne']
 
 
 X = data_2324[features]
@@ -149,20 +159,30 @@ def predict_future_match(h_team, a_team, model, data):
     away_avg_shot = data[data['AwayTeam'] == a_team]['Away_avgShot'].values[-1]
     home_form = data[data['HomeTeam'] == h_team]['home_form'].values[-1]
     away_form = data[data['AwayTeam'] == a_team]['away_form'].values[-1]
+    home_advantage = data[data['HomeTeam'] == h_team]['home_advantage'].values[-1]
+    moyenne_domcile_buts = moyenne_domcile(data)[h_team]
+    moyenne_extérieur_buts = moyenne_exterieru(data)[a_team]
+    difference_moyenne = moyenne_domcile_buts - moyenne_extérieur_buts
+    
 
-    match_features = pd.DataFrame([[home_avg_goal, away_avg_goal, home_avg_shot, away_avg_shot, home_form, away_form]],
+    match_features = pd.DataFrame([[home_avg_goal, away_avg_goal, home_avg_shot, away_avg_shot,
+                                    home_form, away_form, home_advantage, difference_moyenne]],
                                   columns=features)
+    print(match_features)
 
     prediction = model.predict(match_features)[0]
     if prediction == 'H':
         return h_team
     elif prediction == 'A':
         return a_team
+    else:
+        return 'Draw'
+    
 
 
 h_team = str(random.choice(data_2324['HomeTeam'].unique()))
 a_team = str(random.choice(data_2324['AwayTeam'].unique()))
-if home_team != away_team:
+if h_team != a_team:
     predicted_result = predict_future_match(h_team, a_team, model, data_2324)
     print(f"Prédiction pour {h_team} vs {a_team} : {predicted_result}")
 
