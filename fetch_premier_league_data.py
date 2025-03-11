fb3c8e1eb8bed20cca9c1109ade8e359ecd66458
Soglo3006 <ablbooh0@gmail.com>
2025-03-11 03:24:44 -0400

    Ordonner les fonctions

--- fetch_premier_league_data.py
+++ fetch_premier_league_data.py
@@ -24,19 +24,6 @@ def moyenne_Stats(data, team_col, stat_col, new_col):
             data.loc[team_matches.index[i], new_col] = float(window.mean()) 
     return data
 
-data_2324 = moyenne_Stats(data_2324, 'HomeTeam','HomeGoal', 'Home_avgGoal')
-data_2324 = moyenne_Stats(data_2324, 'AwayTeam','AwayGoal', 'Away_avgGoal')
-data_2324 = moyenne_Stats(data_2324, 'HomeTeam','HomeShots', 'Home_avgShot')
-data_2324 = moyenne_Stats(data_2324, 'AwayTeam','AwayShots', 'Away_avgShot')
-data_2324 = moyenne_Stats(data_2324, 'HomeTeam','HCorners', 'Home_avgCorner')
-data_2324 = moyenne_Stats(data_2324, 'AwayTeam','ACorners', 'Away_avgCorner')
-data_2324 = moyenne_Stats(data_2324, 'HomeTeam','HYellow', 'Home_avgYellow')
-data_2324 = moyenne_Stats(data_2324, 'AwayTeam','AYellow', 'Away_avgYellow')
-data_2324 = moyenne_Stats(data_2324, 'HomeTeam','HRed', 'Home_avgRed')
-data_2324 = moyenne_Stats(data_2324, 'AwayTeam','ARed', 'Away_avgRed')
-data_2324 = moyenne_Stats(data_2324, 'HomeTeam','HFouls','Home_avgFouls')
-data_2324 = moyenne_Stats(data_2324, 'AwayTeam','AFouls','Away_avgFouls')
-
 # ligne et colonne
 # row and column
 
@@ -61,46 +48,6 @@ def calculate_form(data, team_col, result_col, new_col):
     data[new_col] = form_list
     return data
 
-data_2324 = calculate_form(data_2324, 'HomeTeam', 'FullTimeResult', 'home_form')
-data_2324 = calculate_form(data_2324, 'AwayTeam', 'FullTimeResult', 'away_form')
-
-features = ['Home_avgGoal','Away_avgGoal','Home_avgShot','Away_avgShot', 'home_form','away_form']
-
-
-X = data_2324[features]
-y = data_2324['FullTimeResult']
-
-# Séparation des données en jeu d'entraînement et de test
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1)
-
-# Entraînement du modèle
-model = RandomForestClassifier(n_estimators=100, min_samples_split=10, random_state=1)
-model.fit(X_train, y_train)
-
-def predict_future_match(home_team, away_team, model, data):
-    if home_team not in data['HomeTeam'].unique():
-        #print(f"Erreur : {home_team} n'existe pas dans les données !")
-        return None
-    if away_team not in data['AwayTeam'].unique():
-        #print(f"Erreur : {away_team} n'existe pas dans les données !")
-        return None
-    
-    home_avg_goal = data[data['HomeTeam'] == home_team]['Home_avgGoal'].values[-1]
-    away_avg_goal = data[data['AwayTeam'] == away_team]['Away_avgGoal'].values[-1]
-    home_avg_shot = data[data['HomeTeam'] == home_team]['Home_avgShot'].values[-1]
-    away_avg_shot = data[data['AwayTeam'] == away_team]['Away_avgShot'].values[-1]
-    home_form = data[data['HomeTeam'] == home_team]['home_form'].values[-1]
-    away_form = data[data['AwayTeam'] == away_team]['away_form'].values[-1]
-
-    match_features = pd.DataFrame([[home_avg_goal, away_avg_goal, home_avg_shot, away_avg_shot, home_form, away_form]],
-                                  columns=features)
-
-    prediction = model.predict(match_features)[0]
-    if prediction == 'H':
-        return home_team
-    elif prediction == 'A':
-        return away_team
-
 def avantageDomicile(data):
     Équipe_victoires_domicile = {}
     for i in data['HomeTeam'].unique():
@@ -117,8 +64,6 @@ def avantageDomicile(data):
         else:
             data.loc[i,'home_advantage'] = 0
 
-#avantageDomicile(data_2324)
-#print(data_2324)
 
 def moyenne_domcile(data):
     Équipe_domicile= {}
@@ -133,9 +78,6 @@ def moyenne_domcile(data):
     for moy in moyenne_buts_domicile:
         moyenne_buts_domicile[moy] = round(moyenne_buts_domicile[moy]/19,2)
     return moyenne_buts_domicile
-        
-        
-#print(moyenne_domcile(data_2324))
 
 def moyenne_exterieru(data):
     Équipe_extérieur = {}
@@ -149,9 +91,6 @@ def moyenne_exterieru(data):
     for moy in moyenne_buts_extérieur :
         moyenne_buts_extérieur[moy] = round(moyenne_buts_extérieur[moy]/19,2)
     return moyenne_buts_extérieur
-        
-        
-#print(moyenne_exterieru(data_2324))
 
 def difference_buts(data, moyenne_dom, moyenne_ext):
     dom = moyenne_dom
@@ -164,14 +103,66 @@ def difference_buts(data, moyenne_dom, moyenne_ext):
             data.loc[i,'difference'] = ext[data.loc[i,'AwayTeam']] - dom[data.loc[i,'HomeTeam']]
             data.loc[i,'difference_nom'] = data.loc[i,'AwayTeam']
 
+data_2324 = moyenne_Stats(data_2324, 'HomeTeam','HomeGoal', 'Home_avgGoal')
+data_2324 = moyenne_Stats(data_2324, 'AwayTeam','AwayGoal', 'Away_avgGoal')
+data_2324 = moyenne_Stats(data_2324, 'HomeTeam','HomeShots', 'Home_avgShot')
+data_2324 = moyenne_Stats(data_2324, 'AwayTeam','AwayShots', 'Away_avgShot')
+data_2324 = moyenne_Stats(data_2324, 'HomeTeam','HCorners', 'Home_avgCorner')
+data_2324 = moyenne_Stats(data_2324, 'AwayTeam','ACorners', 'Away_avgCorner')
+data_2324 = moyenne_Stats(data_2324, 'HomeTeam','HYellow', 'Home_avgYellow')
+data_2324 = moyenne_Stats(data_2324, 'AwayTeam','AYellow', 'Away_avgYellow')
+data_2324 = moyenne_Stats(data_2324, 'HomeTeam','HRed', 'Home_avgRed')
+data_2324 = moyenne_Stats(data_2324, 'AwayTeam','ARed', 'Away_avgRed')
+data_2324 = moyenne_Stats(data_2324, 'HomeTeam','HFouls','Home_avgFouls')
+data_2324 = moyenne_Stats(data_2324, 'AwayTeam','AFouls','Away_avgFouls')
+data_2324 = calculate_form(data_2324, 'HomeTeam', 'FullTimeResult', 'home_form')
+data_2324 = calculate_form(data_2324, 'AwayTeam', 'FullTimeResult', 'away_form')
+
+avantageDomicile(data_2324)
 difference_buts(data_2324,moyenne_domcile(data_2324),moyenne_exterieru(data_2324))
+
 print(data_2324)
+features = ['Home_avgGoal','Away_avgGoal','Home_avgShot','Away_avgShot', 'home_form','away_form']
 
 
-# Exemple de prédiction
-#home_team = str(random.choice(data_2324['HomeTeam'].unique()))
-#away_team = str(random.choice(data_2324['AwayTeam'].unique()))
-#if home_team != away_team:
-#    predicted_result = predict_future_match(home_team, away_team, model, data_2324)
-#    print(f"Prédiction pour {home_team} vs {away_team} : {predicted_result}")
+X = data_2324[features]
+y = data_2324['FullTimeResult']
+
+# Séparation des données en jeu d'entraînement et de test
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1)
+
+# Entraînement du modèle
+model = RandomForestClassifier(n_estimators=100, min_samples_split=10, random_state=1)
+model.fit(X_train, y_train)
+
+def predict_future_match(h_team, a_team, model, data):
+    if h_team not in data['HomeTeam'].unique():
+        #print(f"Erreur : {home_team} n'existe pas dans les données !")
+        return None
+    if a_team not in data['AwayTeam'].unique():
+        #print(f"Erreur : {away_team} n'existe pas dans les données !")
+        return None
+    
+    home_avg_goal = data[data['HomeTeam'] == h_team]['Home_avgGoal'].values[-1]
+    away_avg_goal = data[data['AwayTeam'] == a_team]['Away_avgGoal'].values[-1]
+    home_avg_shot = data[data['HomeTeam'] == h_team]['Home_avgShot'].values[-1]
+    away_avg_shot = data[data['AwayTeam'] == a_team]['Away_avgShot'].values[-1]
+    home_form = data[data['HomeTeam'] == h_team]['home_form'].values[-1]
+    away_form = data[data['AwayTeam'] == a_team]['away_form'].values[-1]
+
+    match_features = pd.DataFrame([[home_avg_goal, away_avg_goal, home_avg_shot, away_avg_shot, home_form, away_form]],
+                                  columns=features)
+
+    prediction = model.predict(match_features)[0]
+    if prediction == 'H':
+        return h_team
+    elif prediction == 'A':
+        return a_team
+
+
+h_team = str(random.choice(data_2324['HomeTeam'].unique()))
+a_team = str(random.choice(data_2324['AwayTeam'].unique()))
+if home_team != away_team:
+    predicted_result = predict_future_match(h_team, a_team, model, data_2324)
+    print(f"Prédiction pour {h_team} vs {a_team} : {predicted_result}")
 
