a2066857960b41fef7f0ce158495573299cf7cb8
Soglo3006 <ablbooh0@gmail.com>
2025-04-01 12:26:49 -0400

    Modification match_process()

--- détails_simulation.py
+++ détails_simulation.py
@@ -139,11 +139,6 @@ def simulate_match(h_team, a_team, prediction_buts_domicile, prediction_buts_ext
 
 #print(simulate_match('Manchester City','Wolves',3,0,60,40,6,13,0,1,0,0,data_joueur_predictions_buteurs))
 
-#minute_buts = set()
-#minutes_changement = []
-minutes_fautes = []
-score_home = 0
-score_away = 0
 home_team_liste = []
 away_team_liste = []
 buteurs_home = []
@@ -158,13 +153,15 @@ red_home_players = []
 red_away_players = []
 yellow_home_players = []
 yellow_away_players = []
+Joueur_remplacer_home = []
+Joueur_rentre_home = []
+Joueur_remplacer_away = []
+Joueur_rentre_away = []
+nb_remplacant_max = 2
 minutes_prises = set()
 minutes_intervalles = [(1, 15), (15, 30), (30, 45), (45, 60), (60, 75), (75, 90)]
 probabilites_minutes = [0.15,0.15,0.20,0.25,0.20,0.15]
 probabilites_minutes_changement = [0.05,0.10,0.20]
-nb_remplacant_max = 2
-Joueur_remplacer_home = []
-Joueur_rentre_home = []
 Hfouls = 2
 Afouls = 0
 HomePossesion = 60
@@ -173,13 +170,12 @@ home_team = 'Manchester City'
 away_team = 'Wolves'
 carton_jaunes_home = 2
 carton_rouges_home = 0
+carton_jaunes_away = 3
+carton_rouges_away = 0
 home_team_liste.append(data_joueur_predictions_buteurs[home_team])
 away_team_liste.append(data_joueur_predictions_buteurs[away_team])
-totalFouls = Hfouls + Afouls
-totalPossesion = HomePossesion + AwayPossesion
-possession = random.choices([home_team, away_team], weights=[HomePossesion/totalPossesion,AwayPossesion/totalPossesion])[0]
-fautes = random.choices([home_team, away_team], weights=[Hfouls/(totalFouls),Afouls/totalFouls])[0]
 prediction_buts_home = 3
+prediction_buts_away = 1
 
 
 """
@@ -196,50 +192,44 @@ while len(minute_buts) < prediction_buts_home:
 minutes_buts = [14, 64, 78]
 minutes_changement = [62, 70]
 minutes_stats = []
-minutes_fautes_cartons_jaunes = [17,65]
+minutes_fautes_cartons_jaunes = [14,65]
 minutes_fautes_cartons_rouges = []
 
-for i in minutes_buts:
-    minutes_stats.append(i)
-for j in minutes_changement:
-    minutes_stats.append(j)
-for z in minutes_fautes_cartons_jaunes:
-    minutes_stats.append(z)
-for l in minutes_fautes_cartons_rouges:
-    minutes_stats.append(l)
-minutes_stats.sort()
-print(minutes_stats)
-for i in minutes_stats:
-    for j in minutes_buts:
-        if i == j:
-            buteur,home_team_liste = stats_Joueur(home_team_liste,'Gls_90','Starting11Players')
-            passeur,home_team_liste = stats_Joueur(home_team_liste,'Ast_90','Starting11Players')
-            while passeur == buteur:
-                passeur = passeur,home_team_liste = stats_Joueur(home_team_liste,'Ast_90','Starting11Players')
-            buteurs_home.append((buteur, i))
-            passeur_home.append((passeur, i))
-    for z in minutes_fautes_cartons_jaunes:
-        if i == z:
-            Joueur_carton_jaune,Home_team_liste = stats_Joueur(home_team_liste,'CrdYAvg','Starting11Players')
-            yellow_home_players.append((Joueur_carton_jaune,z))
-            
-    for l in minutes_fautes_cartons_rouges:
-        if i == l:
-            Joueur_carton_rouge,Home_team_liste = stats_Joueur(home_team_liste,'CrdRAvg','Starting11Players')
-            red_home_players.append((Joueur_carton_rouge,l))
-    for k in minutes_changement:
-        if i == k:
-            SPlayer, home_team_liste = stats_Joueur(home_team_liste,'ProbOut','Starting11Players')
-            BPlayer, home_team_liste = stats_Joueur(home_team_liste,'ProbFinal','BenchPlayers')
-            resultat,updateteam  = changement_de_joueur(data_joueur_predictions_buteurs,home_team,SPlayer,BPlayer)
-            home_team_liste = updateteam
-            resultat = resultat[0]['Player']
-            Joueur_remplacer_home.append((SPlayer,k))
-            Joueur_rentre_home.append((BPlayer,k))
+minutes_stats = sorted(set(minutes_buts + minutes_changement + minutes_fautes_cartons_jaunes + minutes_fautes_cartons_rouges))
 
-print(buteurs_home)
-print(passeur_home)
-print(yellow_home_players)
-print(red_home_players)
-print(Joueur_remplacer_home)
-print(Joueur_rentre_home)
+print(minutes_stats)
+
+def match_process(prediction_buts_team,team_liste,cartons_jaunes_team,cartons_rouges_team,remplacant_nb,team,buteur_team,passeur_team,yellow_team,red_team,Joueur_remplacer_team,Joueur_rentre_team):
+    for i in minutes_stats:
+        for j in minutes_buts:
+            if i == j and len(buteur_team) < prediction_buts_team:
+                buteur,team_liste = stats_Joueur(team_liste,'Gls_90','Starting11Players')
+                passeur,team_liste = stats_Joueur(team_liste,'Ast_90','Starting11Players')
+                while passeur == buteur:
+                    passeur = passeur,team_liste = stats_Joueur(team_liste,'Ast_90','Starting11Players')
+                buteur_team.append((buteur, i))
+                passeur_team.append((passeur, i))
+        for z in minutes_fautes_cartons_jaunes: 
+            if i == z and len(yellow_team) < cartons_jaunes_team:
+                Joueur_carton_jaune,team_liste = stats_Joueur(team_liste,'CrdYAvg','Starting11Players')
+                yellow_team.append((Joueur_carton_jaune,z))
+                
+        for l in minutes_fautes_cartons_rouges :
+            if i == l and len(red_team) < cartons_rouges_team :
+                Joueur_carton_rouge,team_liste = stats_Joueur(team_liste,'CrdRAvg','Starting11Players')
+                red_team.append((Joueur_carton_rouge,l))
+        for k in minutes_changement:
+            if i == k and len(Joueur_remplacer_team) < remplacant_nb :
+                SPlayer, team_liste = stats_Joueur(team_liste,'ProbOut','Starting11Players')
+                BPlayer, team_liste = stats_Joueur(team_liste,'ProbFinal','BenchPlayers')
+                resultat,updateteam  = changement_de_joueur(data_joueur_predictions_buteurs,team,SPlayer,BPlayer)
+                team_liste = updateteam
+                resultat = resultat[0]['Player']
+                Joueur_remplacer_team.append((SPlayer,k))
+                Joueur_rentre_team.append((BPlayer,k))
+    return buteur_team, passeur_team, yellow_team, red_team, Joueur_remplacer_team, Joueur_rentre_team
+
+print(match_process(prediction_buts_home,home_team_liste,carton_jaunes_home,carton_rouges_home,nb_remplacant_max,
+                    home_team,buteurs_home,passeur_home,yellow_home_players,red_home_players,Joueur_remplacer_home,Joueur_rentre_home))
+print(match_process(prediction_buts_away,away_team_liste,carton_jaunes_away,carton_rouges_away,nb_remplacant_max,
+                    away_team,buteurs_away,passeur_away,yellow_away_players,red_away_players,Joueur_remplacer_away,Joueur_rentre_away))
