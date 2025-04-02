4cce553a29745383413cfb64dbff5fe05d01caf3
Soglo3006 <ablbooh0@gmail.com>
2025-04-02 09:39:58 -0400

    modification model.py

--- model.py
+++ model.py
@@ -1,15 +1,15 @@
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor, RandomForestClassifier
 
-def train_models(data, features, teamCategorie):
+def train_models(data, features, team_categorie):
     X = data[features]
-    y_home = data[teamCategorie]
-    X_train, X_test, y_train, y_test = train_test_split(X, y_home, test_size=0.2, random_state=1)
-    if teamCategorie == 'HomeGoal' or teamCategorie == 'AwayGoal' or teamCategorie == 'HYellow' or teamCategorie == 'AYellow' or teamCategorie == 'HRed' or teamCategorie == 'ARed':
+    y_home = data[team_categorie]
+    x_train, x_test, y_train, y_test = train_test_split(X, y_home, test_size=0.2, random_state=1)
+    if team_categorie == 'HomeGoal' or team_categorie == 'AwayGoal' or team_categorie == 'HYellow' or team_categorie == 'AYellow' or team_categorie == 'HRed' or team_categorie == 'ARed':
         model = RandomForestClassifier(n_estimators=500, max_depth=7, random_state=1)
-        model.fit(X_train, y_train)    
+        model.fit(x_train, y_train)    
     else:
         model = RandomForestRegressor(n_estimators=1500, max_depth=15, min_samples_split=10, random_state=1)
-        model.fit(X_train, y_train)
+        model.fit(x_train, y_train)
 
     return model
