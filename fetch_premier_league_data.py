03f80cd1f136daba2a26aab17792fa5c11c7402e
Soglo3006 <ablbooh0@gmail.com>
2025-04-03 07:49:26 -0400

    modification des noms

--- fetch_premier_league_data.py
+++ fetch_premier_league_data.py
@@ -4,31 +4,31 @@ data_2324 = pd.read_csv('fichier csv/season-2324.csv')
 match_2324 = pd.read_csv('fichier csv/matches-23-24.csv')
 
 def moyenne_stats(data,h_team,a_team,h_categore,a_categorie,home_new_col,away_new_col):
-    equipe = {}
-    for i in data[h_team].unique():
-        equipe[i] = 0 
-        equipe = dict(sorted(equipe.items()))
-    for y in range(len(data)):
-        for j in equipe:
-            if data.loc[y,h_team] == j :
-                equipe[j] += int(data.loc[y,h_categore])
-            elif data.loc[y,a_team] == j:
-                equipe[j] += int(data.loc[y,a_categorie])
-    for k in equipe:
-        equipe[k] = round(equipe[k]/38,2)
-    for z in range(len(data)):
-        for a in equipe:
-            if a == data.loc[z,h_team]:
-                data.loc[z,home_new_col] = equipe[a]
-            elif a == data.loc[z,a_team]:
-                data.loc[z,away_new_col] = equipe[a]
-    return equipe
+    teams = {}
+    for team_name in data[h_team].unique():
+        teams[team_name] = 0 
+        teams = dict(sorted(teams.items()))
+    for match_index in range(len(data)):
+        for team in teams:
+            if data.loc[match_index,h_team] == team :
+                teams[team] += int(data.loc[match_index,h_categore])
+            elif data.loc[match_index,a_team] == team:
+                teams[team] += int(data.loc[match_index,a_categorie])
+    for team in teams:
+        teams[team] = round(teams[team]/38,2)
+    for match_index in range(len(data)):
+        for a in teams:
+            if a == data.loc[match_index,h_team]:
+                data.loc[match_index,home_new_col] = teams[a]
+            elif a == data.loc[match_index,a_team]:
+                data.loc[match_index,away_new_col] = teams[a]
+    return teams
 
 def calculate_form(data, team_col, result_col, new_col):
     form_list = []
-    for i in range(len(data)):
-        team = data.loc[i, team_col]
-        past_matches = data.loc[:i-1]
+    for match_index in range(len(data)):
+        team = data.loc[match_index, team_col]
+        past_matches = data.loc[:match_index-1]
         team_matches = past_matches[past_matches[team_col] == team]
 
         form_score = 0
@@ -46,56 +46,56 @@ def calculate_form(data, team_col, result_col, new_col):
     return data
 
 def avantage_domicile(data):
-    equipe_victoires_domicile = {}
-    for i in data['HomeTeam'].unique():
-        equipe_victoires_domicile[i] = 0
-    for y in range(len(data)):
-        if data.loc[y,'FullTimeResult'] == 'H':
-            equipe_victoires_domicile[data.loc[y,'HomeTeam']] += 1
-    for j in equipe_victoires_domicile:
-        equipe_victoires_domicile[j] = round(equipe_victoires_domicile[j]/19,2)
-    porucentage_victoire = dict(sorted(equipe_victoires_domicile.items()))
-    for i in range(len(data)):
-        if porucentage_victoire[data.loc[i,'HomeTeam']] > porucentage_victoire[data_2324.loc[i,'AwayTeam']]:
-            data.loc[i,'home_advantage'] = 1
+    home_team_wins = {}
+    for team_name in data['HomeTeam'].unique():
+        home_team_wins[team_name] = 0
+    for match_index in range(len(data)):
+        if data.loc[match_index,'FullTimeResult'] == 'H':
+            home_team_wins[data.loc[match_index,'HomeTeam']] += 1
+    for team in home_team_wins:
+        home_team_wins[team] = round(home_team_wins[team]/19,2)
+    win_percentage = dict(sorted(home_team_wins.items()))
+    for match_index in range(len(data)):
+        if win_percentage[data.loc[match_index,'HomeTeam']] > win_percentage[data_2324.loc[match_index,'AwayTeam']]:
+            data.loc[match_index,'home_advantage'] = 1
         else:
-            data.loc[i,'home_advantage'] = 0
+            data.loc[match_index,'home_advantage'] = 0
 
 
-def moyenne_stats_buts(data,equipe,but,new_col):
-    stats_equipe = {}
-    for i in data[equipe].unique():
-        stats_equipe[i] = 0
-        stats_equipe = dict(sorted(stats_equipe.items()))
-    for y in range(len(data)):
-        for j in stats_equipe:
-            if data.loc[y,equipe] == j:
-                stats_equipe[j] += int(data.loc[y,but])
-    for moy in stats_equipe:
-        stats_equipe[moy] = round(stats_equipe[moy]/19,2)
-    for k in range(len(data)):
-        for nom_equipe in stats_equipe:
-            if nom_equipe == data.loc[k,equipe]:
-                data.loc[k,new_col] = stats_equipe[nom_equipe]
-    return stats_equipe
-def difference_buts(data, moyenne_dom, moyenne_ext, new_col, new_col2):
-    dom = moyenne_dom
-    ext = moyenne_ext
-    for i in range(len(data)):
-        if dom[data.loc[i,'HomeTeam']]> ext[data.loc[i,'AwayTeam']] : 
-            data.loc[i,new_col] = dom[data.loc[i,'HomeTeam']] - ext[data.loc[i,'AwayTeam']]
-            data.loc[i,new_col2] = data.loc[i,'HomeTeam']
+def moyenne_stats_buts(data,equipe,goal,new_col):
+    team_stats = {}
+    for team_name in data[equipe].unique():
+        team_stats[team_name] = 0
+        team_stats = dict(sorted(team_stats.items()))
+    for match_index in range(len(data)):
+        for j in team_stats:
+            if data.loc[match_index,equipe] == j:
+                team_stats[j] += int(data.loc[match_index,goal])
+    for moy in team_stats:
+        team_stats[moy] = round(team_stats[moy]/19,2)
+    for match_index in range(len(data)):
+        for nom_equipe in team_stats:
+            if nom_equipe == data.loc[match_index,equipe]:
+                data.loc[match_index,new_col] = team_stats[nom_equipe]
+    return team_stats
+def difference_buts(data, home_average, away_average, new_col, new_col2):
+    home = home_average
+    away = away_average
+    for match_index in range(len(data)):
+        if home[data.loc[match_index,'HomeTeam']]> away[data.loc[match_index,'AwayTeam']] : 
+            data.loc[match_index,new_col] = home[data.loc[match_index,'HomeTeam']] - away[data.loc[match_index,'AwayTeam']]
+            data.loc[match_index,new_col2] = data.loc[match_index,'HomeTeam']
         else:
-            data.loc[i,new_col] = ext[data.loc[i,'AwayTeam']] - dom[data.loc[i,'HomeTeam']]
-            data.loc[i,new_col2] = data.loc[i,'AwayTeam']
+            data.loc[match_index,new_col] = away[data.loc[match_index,'AwayTeam']] - home[data.loc[match_index,'HomeTeam']]
+            data.loc[match_index,new_col2] = data.loc[match_index,'AwayTeam']
 
 def format_date(match_df):
-    for i in range(len(match_df)):
-        date = match_df.loc[i, 'Date']
-        annee = date[2:4] 
-        mois = date[5:7]  
-        jour = date[8:10]  
-        match_df.loc[i, 'Date'] = f"{jour}/{mois}/{annee}"
+    for match_index in range(len(match_df)):
+        date = match_df.loc[match_index, 'Date']
+        year = date[2:4] 
+        month = date[5:7]  
+        day = date[8:10]  
+        match_df.loc[match_index, 'Date'] = f"{day}/{month}/{year}"
 
     match_df['Date'] = pd.to_datetime(match_df['Date'], format='%d/%m/%y')
     match_df = match_df.sort_values(by='Date').reset_index(drop=True)
@@ -104,26 +104,26 @@ def format_date(match_df):
     return match_df
 
 def correct_team_names(match_df):
-    corrections = {
+    fixing_name = {
         'Luton Town': 'Luton',
         'Newcastle Utd': 'Newcastle',
         'Sheffield Utd': 'Sheffield United',
         'Manchester Utd': 'Manchester United',
         "Nott'ham Forest": 'Nottingham Forest'
     }
-    match_df['Opponent'] = match_df['Opponent'].replace(corrections)
+    match_df['Opponent'] = match_df['Opponent'].replace(fixing_name)
 
     return match_df
 
 def team_stats(match_df, data_df,new_col1, new_col2,new_col3,new_col4):
-    for k in range(len(match_df)):
-        for h in range(len(data_df)):
-            if match_df.loc[k, 'Date'] == data_df.loc[h, 'Date']:
-                if match_df.loc[k, 'Opponent'] == data_df.loc[h, 'HomeTeam']:
-                    data_df.loc[h, new_col2] = 100 - match_df.loc[k, 'Poss']
-                    data_df.loc[h, new_col1] = 100 - data_df.loc[h, new_col2]
-                    data_df.loc[h,new_col3] = match_df.loc[k,'xGA']
-                    data_df.loc[h,new_col4] = match_df.loc[k,'xG']
+    for match_index in range(len(match_df)):
+        for match_index_in_data in range(len(data_df)):
+            if match_df.loc[match_index, 'Date'] == data_df.loc[match_index_in_data, 'Date']:
+                if match_df.loc[match_index, 'Opponent'] == data_df.loc[match_index_in_data, 'HomeTeam']:
+                    data_df.loc[match_index_in_data, new_col2] = 100 - match_df.loc[match_index, 'Poss']
+                    data_df.loc[match_index_in_data, new_col1] = 100 - data_df.loc[match_index_in_data, new_col2]
+                    data_df.loc[match_index_in_data,new_col3] = match_df.loc[match_index,'xGA']
+                    data_df.loc[match_index_in_data,new_col4] = match_df.loc[match_index,'xG']
 
     return data_df
 
